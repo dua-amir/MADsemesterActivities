4ffd857c5c3285f86d40a5fd6e571adc154a3f51
dua-amir <duaamir4211@gmail.com>
2025-03-05 17:28:16 +0500

    week4 practice

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput} from 'react-native';
 import { Header } from './components/Header.js';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Header name = 'top component'></Header>
       <Text>Hello Dua, Congratulations your first app is running successfully!</Text>
-      <Header>Heyyyyyyyyyy</Header>
+      <Header name = 'bottom component'></Header>
+      <TextInput placeholder='typing....'></TextInput>
     </View>
   );
 }
